39d0c860e7bf476bcf0a57935133e025bf2f38b3
ottotuhkunen <otto.tuhkunen@hotmail.com>
2023-07-10 01:04:29 +0300

    Added authentication

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const basicAuth = require('express-basic-auth')
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(basicAuth({
-    users: { 'vatscaEFIN': 'HelsinkiAWOS2023!' }, // username, password
+    users: { [process.env.BASIC_AUTH_USER]: process.env.BASIC_AUTH_PASSWORD },
     challenge: true
 }));
 
 app.use(express.static('.')); // index.html directory
 
-app.listen(3000, function () {
-    console.log('App is listening on port 3000!');
+app.listen(port, function () {
+    console.log('App is listening on port ' + port + '!');
 });
