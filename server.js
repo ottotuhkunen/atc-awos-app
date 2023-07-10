8abc963fae8c93614bbf82d0c9a7b69f04c489df
ottotuhkunen <otto.tuhkunen@hotmail.com>
2023-07-10 12:27:39 +0300

    add https forcing

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const basicAuth = require('express-basic-auth')
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https') {
+      res.redirect(`https://${req.header('host')}${req.url}`);
+    } else {
+      next();
+    }
+});
+
 app.use(basicAuth({
     users: { [process.env.BASIC_AUTH_USER]: process.env.BASIC_AUTH_PASSWORD },
     challenge: true
